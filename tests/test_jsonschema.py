4a23f96c0b20933696e1446b2f9c8b22fa039ec1
Dominic Parent <dominic.parent@canada.ca>
2021-03-09 12:22:40 -0500

    Added validation of json schema.

---
+++ tests/test_jsonschema.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © Her Majesty the Queen in Right of Canada, as represented
+# by the Minister of Statistics Canada, 2020.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+import json
+from jsonschema import Draft7Validator
+
+
+class JsonSchemaTestCase(unittest.TestCase):
+    def setUp(self):
+        print("+++++++++++++++++++++++++++++++++++++++")
+        print("Begining new TestCase %s" % self._testMethodName)
+        print("+++++++++++++++++++++++++++++++++++++++")
+
+        with open("pyproddi/io/json/ddicdi.json") as f:
+            self.schema = json.load(f)
+
+    def test_JsonSchemaValidate(self):
+        print(self.schema)
+
+if __name__ == "__main__":
+    unittest.main()
